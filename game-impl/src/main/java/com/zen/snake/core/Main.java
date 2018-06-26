3aaabec4faf052d7095e550c2ad3be6ea436e803
Luca Zenobi <luzenobi@gmail.com>
2018-06-26 22:34:06 +0200

    Minor updates

--- game-impl/src/main/java/com/zen/snake/core/Main.java
+++ game-impl/src/main/java/com/zen/snake/core/Main.java
@@ -1,5 +1,6 @@
 package com.zen.snake.core;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 
 public class Main {
@@ -15,19 +16,23 @@ public class Main {
 		start();
 	}
 
-	private void startInterface() throws Exception {
+	private void startInterface() {
     window = new WindowInterface();
-    context = (Graphics2D) window.getjFrame().getGraphics();
+    context = (Graphics2D) window.getJFrame().getGraphics();
 	}
 
 	private void startInputListeners() {
-    window.getjFrame().addMouseListener(mouseListenerHome);
+    window.getJFrame().addMouseListener(mouseListenerHome);
+  }
+
+  private void clearScreen() {
+    context.setColor(Color.BLACK);
+    context.fillRect(0, 0, 800, 450);
   }
 
 	private void start() throws InterruptedException {
 		while(running) {
-
-
+      clearScreen();
 		  Thread.sleep(16);
 		}
 	}
--- game-impl/src/main/java/com/zen/snake/core/WindowInterface.java
+++ game-impl/src/main/java/com/zen/snake/core/WindowInterface.java
@@ -7,22 +7,14 @@ public class WindowInterface {
 
 	private JFrame jFrame;
 
-	public WindowInterface() throws Exception {
+	public WindowInterface() {
 		jFrame = new JFrame("Snake Test");
 		jFrame.setSize(800, 450);
 		jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-		/*WindowElements windowElements = new WindowElements();
-
-		jFrame.add(windowElements);
-		
-		MouseListenerHome mouseListenerHome = new MouseListenerHome();
-		jFrame.addMouseListener(mouseListenerHome);*/
-
 		jFrame.setVisible(true);
 	}
 
-	public JFrame getjFrame() {
+	public JFrame getJFrame() {
 		return jFrame;
 	}
 
