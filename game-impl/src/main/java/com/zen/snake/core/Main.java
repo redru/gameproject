59abfbcb92ceb68536a518055ee21e6ab5491950
Luca Zenobi <luzenobi@gmail.com>
2018-06-26 22:28:08 +0200

    Minor updates

--- game-impl/src/main/java/com/zen/snake/core/Main.java
+++ game-impl/src/main/java/com/zen/snake/core/Main.java
@@ -1,19 +1,28 @@
 package com.zen.snake.core;
 
+import java.awt.Graphics2D;
+
 public class Main {
 	
 	private boolean running = true;
 	private MouseListenerHome mouseListenerHome = new MouseListenerHome();
 	private WindowInterface window;
+	private Graphics2D context;
 
 	private Main() throws Exception {
 		startInterface();
+    startInputListeners();
 		start();
 	}
 
 	private void startInterface() throws Exception {
     window = new WindowInterface();
+    context = (Graphics2D) window.getjFrame().getGraphics();
 	}
+
+	private void startInputListeners() {
+    window.getjFrame().addMouseListener(mouseListenerHome);
+  }
 
 	private void start() throws InterruptedException {
 		while(running) {
